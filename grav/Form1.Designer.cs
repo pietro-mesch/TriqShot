679bff5913ef6584dea1d3bf6bf80fc92d092fa8
pietro-mesch <mezkaster@gmail.com>
2022-11-25 13:36:34 +0100

    new button

--- grav/Form1.Designer.cs
+++ grav/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace grav
         private void InitializeComponent()
         {
             this.gameView = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // gameView
@@ -51,12 +52,25 @@ namespace grav
             this.gameView.TabIndex = 0;
             this.gameView.Paint += new System.Windows.Forms.PaintEventHandler(this.GameView_Paint);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Orator Std", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(8, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "NEW GAME";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.gameView);
             this.ForeColor = System.Drawing.Color.Lime;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -71,6 +85,7 @@ namespace grav
         #endregion
 
         private Panel gameView;
+        private Button button1;
     }
 }
 
